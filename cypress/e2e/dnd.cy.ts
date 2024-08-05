55e54143123b92e419fadb7e5c9b546015e2c03d
Sumin <csmxx99@naver.com>
2024-08-05 23:55:22 +0900

    test: Column-1에서 Column-3로의 이동 금지 테스트 추가

--- cypress/e2e/dnd.cy.ts
+++ cypress/e2e/dnd.cy.ts
@@ -11,13 +11,7 @@ describe('DnD 구현 과제 요구사항 테스트', () => {
     cy.get('#columnCountUp').click();
   });
 
-  afterEach(() => {
-    cy.get('#columnCountDown').click();
-    cy.get('#columnCountDown').click();
-    cy.get('#columnCountDown').click();
-  });
-
-  it('하나의 column 내에서 DnD 가능', () => {
+  it('하나의 Column 내에서 DnD 가능', () => {
     cy.get('#item-0')
       .trigger('keydown', { keyCode: keyCodes.space })
       .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
@@ -30,7 +24,7 @@ describe('DnD 구현 과제 요구사항 테스트', () => {
     cy.get('#item-0').next().should('contain', 'item 5');
   });
 
-  it('하나의 column에서 다른 여러 칼럼으로 DnD 가능', () => {
+  it('하나의 Column에서 다른 여러 칼럼으로 DnD 가능', () => {
     cy.get('#item-0')
       .trigger('keydown', { keyCode: keyCodes.space })
       .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
@@ -40,6 +34,11 @@ describe('DnD 구현 과제 요구사항 테스트', () => {
     cy.get('#item-2')
       .trigger('keydown', { keyCode: keyCodes.space })
       .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-0')
+      .trigger('keydown', { keyCode: keyCodes.space })
       .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
       .wait(timings.outOfTheWay * 1000)
       .trigger('keydown', { keyCode: keyCodes.space, force: true });
@@ -56,8 +55,39 @@ describe('DnD 구현 과제 요구사항 테스트', () => {
     cy.get('#Column-1').should('not.contain', 'item 2');
     cy.get('#Column-1').should('not.contain', 'item 5');
 
-    cy.get('#Column-2').should('contain', 'item 0');
-    cy.get('#Column-3').should('contain', 'item 2');
+    cy.get('#Column-2').should('contain', 'item 2');
+    cy.get('#Column-3').should('contain', 'item 0');
     cy.get('#Column-4').should('contain', 'item 5');
+  });
+
+  it('Column-1에서 Column-3으로 이동 불가', () => {
+    cy.get('#item-0')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-2')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-6')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#Column-3').should('not.contain', 'item 0');
+    cy.get('#Column-3').should('not.contain', 'item 2');
+    cy.get('#Column-3').should('not.contain', 'item 6');
+
+    cy.get('#Column-1').should('contain', 'item 0');
+    cy.get('#Column-1').should('contain', 'item 2');
+    cy.get('#Column-1').should('contain', 'item 6');
   });
 });
