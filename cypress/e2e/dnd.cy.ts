77ffe0203021dd0447f3a732ba9ec5ea980e92e1
Sumin <csmxx99@naver.com>
2024-08-06 02:59:06 +0900

    test: 짝수 아이템은 다른 짝수 아이템 앞으로 이동 금지 테스트 추가

--- cypress/e2e/dnd.cy.ts
+++ cypress/e2e/dnd.cy.ts
@@ -90,4 +90,99 @@ describe('DnD 구현 과제 요구사항 테스트', () => {
     cy.get('#Column-1').should('contain', 'item 2');
     cy.get('#Column-1').should('contain', 'item 6');
   });
+
+  it('같은 Column 내에서 짝수 위치 아이템을 짝수 위치 아이템 앞 이동 금지', () => {
+    cy.get('#item-1')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-0').next().should('contain', 'item 1');
+  });
+
+  it('다른 Column으로 이동 시 짝수 위치 아이템을 짝수 위치 아이템 앞 이동 금지', () => {
+    cy.get('#item-0')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-1')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-2')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-3')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-5')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#Column-2').should('not.contain', 'item 5');
+    cy.get('#Column-1').should('contain', 'item 5');
+  });
+
+  it('짝수 위치 아이템을 짝수 위치의 마지막 요소로 이동 가능', () => {
+    cy.get('#item-0')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-1')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-2')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#item-4')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#Column-1').should('not.contain', 'item 4');
+    cy.get('#Column-2').should('contain', 'item 4');
+  });
+
+  it('짝수 위치 아이템을 Drag해서 원래 위치로 Drop 가능', () => {
+    cy.get('#item-1')
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowDown, force: true })
+      .wait(timings.outOfTheWay * 1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.get('#Column-1').should('contain', 'item 1');
+  });
 });
